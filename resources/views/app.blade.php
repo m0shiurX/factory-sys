b43cfbdbfa77e99aaf5a5df2a708aacd62445de3
Moshiur Rahman <10681932+m0shiurX@users.noreply.github.com>
2025-12-06 19:00:17 +0600

    removed clarity

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -39,39 +39,6 @@
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @inertiaHead
-
-        <!-- Google tag (gtag.js) -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-28VSRT7SCS"></script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-28VSRT7SCS');
-            </script>
-        <!-- End Google Tag Manager -->
-        <!-- MicroSoft Clarity -->
-        <script type="text/javascript">
-            (function(c,l,a,r,i,t,y){
-                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-            })(window, document, "clarity", "script", "u8jmvs22xy");
-        </script>
-        <!-- End MicroSoft Clarity -->
-        <!-- Meta Pixel Code -->
-        <script>
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '743471802112206');
-            fbq('track', 'PageView');
-        </script>
-        <!-- End Meta Pixel Code -->
     </head>
     <body class="font-sans antialiased">
         @inertia
